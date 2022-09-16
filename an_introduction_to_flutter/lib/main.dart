ae3b26cd370543b15de377c809d1d0aed395f227
Yasser Farahi <yfarahi@risinggalaxy.com>
2022-09-16 11:44:30 +0200

    added new lines

--- an_introduction_to_flutter/lib/main.dart
+++ an_introduction_to_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,8 +25,29 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
-  final bool _buttonWasTapped = false;
-  final String _buttonTitle = 'Launch Ship'.toUpperCase();
+  bool _buttonWasTapped = false;
+  String _buttonTitle = 'Launch Ship'.toUpperCase();
+  final AudioPlayer _backgroundPlayer = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _backgroundPlayer.setVolume(0.5);
+    playAudio(
+      _backgroundPlayer,
+      'spaceBackground.wav',
+    );
+  }
+
+  void playAudio(AudioPlayer player, String sourcePath) async {
+    await player.setSource(AssetSource('sound/$sourcePath'));
+  }
+
+  @override
+  void dispose() {
+    _backgroundPlayer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,9 +71,16 @@ class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
                   alignment: _buttonWasTapped
                       ? Alignment.topCenter
                       : Alignment.bottomCenter,
-                  height: MediaQuery.of(context).size.height * 1.2,
+                  height: MediaQuery.of(context).size.height * 1.1,
                   duration: const Duration(seconds: 3),
                   curve: Curves.easeInOut,
+                  onEnd: () {
+                    setState(() {
+                      _buttonTitle = _buttonWasTapped
+                          ? 'Land Ship'.toUpperCase()
+                          : 'Launch Ship'.toUpperCase();
+                    });
+                  },
                   child: Image.asset(
                     'images/spaceShip.png',
                     height: 200.0,
@@ -68,33 +97,33 @@ class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
                       ),
                     ),
                     GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _buttonWasTapped = !_buttonWasTapped;
-                        });
-                      },
-                      child: const SizedBox(
-                        height: 40.0,
-                        width: 350.0,
+                      child: SizedBox(
+                        height: 50.0,
+                        width: 300.0,
                         child: DecoratedBox(
                           decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 30, 40, 50),
-                            borderRadius: BorderRadius.all(
+                            color: Colors.blue.shade600,
+                            borderRadius: const BorderRadius.all(
                               Radius.circular(25.0),
                             ),
                           ),
                           child: Center(
                             child: Text(
-                              'SpaceX Ship',
-                              style: TextStyle(
+                              _buttonTitle,
+                              style: const TextStyle(
                                 color: Colors.white,
-                                fontSize: 25.0,
+                                fontSize: 20.0,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
                         ),
                       ),
+                      onTap: () {
+                        setState(() {
+                          _buttonWasTapped = !_buttonWasTapped;
+                        });
+                      },
                     ),
                   ],
                 ),
